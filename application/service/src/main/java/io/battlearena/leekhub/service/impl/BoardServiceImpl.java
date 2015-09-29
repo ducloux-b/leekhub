d0a2ceca3a46fdfa19675bf840fe6c482fcf3bfa
Thomah <thomas.hingant@laposte.net>
2015-09-29 20:53:24 +0200

    Travail sur les services

--- application/service/src/main/java/io/battlearena/leekhub/service/impl/BoardServiceImpl.java
+++ application/service/src/main/java/io/battlearena/leekhub/service/impl/BoardServiceImpl.java
@@ -1,6 +1,9 @@
 package io.battlearena.leekhub.service.impl;
 
+import com.google.gson.Gson;
+
 import io.battlearena.client.BattlearenaIo_TestWs.Root;
+import io.battlearena.client.BattlearenaIo_TestWs.Root.GameBoardIdPartie;
 import io.battlearena.leekhub.model.entity.Board;
 import io.battlearena.leekhub.service.BoardService;
 import io.battlearena.leekhub.singleton.ClientSingleton;
@@ -10,7 +13,10 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public Board getBoard(String idPartie) {
+		Gson gson = new Gson();
 		Root root = RootSingleton.getInstance(ClientSingleton.getInstance());
+		GameBoardIdPartie board = root.gameBoardIdPartie(idPartie);
+		Board b = gson.fromJson(board.getAs(String.class), Board.class);
 		return null;
 	}
 	

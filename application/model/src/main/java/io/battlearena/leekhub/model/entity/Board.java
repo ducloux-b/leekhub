62e09fe4a7c6a290a2452be5ff7b49463c055ecc
Nacrotic <leo.perrin@gmail.com>
2015-09-30 18:49:51 +0200

    Correction Model

--- application/model/src/main/java/io/battlearena/leekhub/model/entity/Board.java
+++ application/model/src/main/java/io/battlearena/leekhub/model/entity/Board.java
@@ -3,9 +3,7 @@ package io.battlearena.leekhub.model.entity;
 public class Board {
 	Player player1;
 	Player player2;
-	Player player3;
-	Player player4;
-	int longueur;
+	int nbrActionLeft;
 	int largeur;
 	
 	public Board() {
@@ -24,37 +22,27 @@ public class Board {
 	public final void setPlayer2(Player player2) {
 		this.player2 = player2;
 	}
-	public final Player getPlayer3() {
-		return player3;
+
+	public final int getNbrActionLeft() {
+		return nbrActionLeft;
 	}
-	public final void setPlayer3(Player player3) {
-		this.player3 = player3;
+
+	public final void setNbrActionLeft(int nbrActionLeft) {
+		this.nbrActionLeft = nbrActionLeft;
 	}
-	public final Player getPlayer4() {
-		return player4;
-	}
-	public final void setPlayer4(Player player4) {
-		this.player4 = player4;
-	}
-	public final int getLongueur() {
-		return longueur;
-	}
-	public final void setLongueur(int longueur) {
-		this.longueur = longueur;
-	}
+
 	public final int getLargeur() {
 		return largeur;
 	}
+
 	public final void setLargeur(int largeur) {
 		this.largeur = largeur;
 	}
 
 	@Override
 	public String toString() {
-		return "Board [player1=" + player1.toString() + ", player2=" + player2.toString() + ", player3=" + player3.toString() + ", player4=" + player4.toString()
-				+ ", longueur=" + longueur + ", largeur=" + largeur + "]";
+		return "Board [player1=" + player1 + ", player2=" + player2 + ", nbrActionLeft=" + nbrActionLeft + ", largeur="
+				+ largeur + "]";
 	}
-	
-	
 
 }
--- application/model/src/main/java/io/battlearena/leekhub/model/entity/Player.java
+++ application/model/src/main/java/io/battlearena/leekhub/model/entity/Player.java
@@ -1,37 +1,61 @@
 package io.battlearena.leekhub.model.entity;
 
 public class Player {
-	String nom;
-	int nbrDePieces;
-	String dernierMouvement;
+	String name;
+	int health;
+	int bullet;
+	int shield;
+	boolean focused;
 	
 	public Player() {
 		super();
 	}
-	
-	public final String getNom() {
-		return nom;
+
+	public final String getName() {
+		return name;
 	}
-	public final void setNom(String nom) {
-		this.nom = nom;
+
+	public final void setName(String name) {
+		this.name = name;
 	}
-	public final int getNbrDePieces() {
-		return nbrDePieces;
+
+	public final int getHealth() {
+		return health;
 	}
-	public final void setNbrDePieces(int nbrDePieces) {
-		this.nbrDePieces = nbrDePieces;
+
+	public final void setHealth(int health) {
+		this.health = health;
 	}
-	public final String getDernierMouvement() {
-		return dernierMouvement;
+
+	public final int getBullet() {
+		return bullet;
 	}
-	public final void setDernierMouvement(String dernierMouvement) {
-		this.dernierMouvement = dernierMouvement;
+
+	public final void setBullet(int bullet) {
+		this.bullet = bullet;
+	}
+
+	public final int getShield() {
+		return shield;
+	}
+
+	public final void setShield(int shield) {
+		this.shield = shield;
+	}
+
+	public final boolean isFocused() {
+		return focused;
+	}
+
+	public final void setFocused(boolean focused) {
+		this.focused = focused;
 	}
 
 	@Override
 	public String toString() {
-		return "Player [nom=" + nom + ", nbrDePieces=" + nbrDePieces + ", dernierMouvement=" + dernierMouvement + "]";
+		return "Player [name=" + name + ", health=" + health + ", bullet=" + bullet + ", shield=" + shield
+				+ ", focused=" + focused + "]";
 	}
 	
-	
+
 }

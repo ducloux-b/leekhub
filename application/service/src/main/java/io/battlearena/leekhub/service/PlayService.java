eccef0cc5d7c9db40a70218c20e71dd837631a84
Thomah <thomas.hingant@laposte.net>
2015-09-30 18:35:42 +0200

    blzabla

--- application/service/src/main/java/io/battlearena/leekhub/service/PlayService.java
+++ application/service/src/main/java/io/battlearena/leekhub/service/PlayService.java
@@ -10,16 +10,3 @@ import io.battlearena.leekhub.model.entity.PlayStatus;
 public interface PlayService {
 	PlayStatus getPlayStatus();
 }
-=======
-package io.battlearena.leekhub.service;
-
-import io.battlearena.leekhub.model.entity.PlayStatus;
-
-/**
- * Definitif
- * @author leperrin
- *
- */
-public interface PlayService {
-	PlayStatus getPlayStatus();
-}
